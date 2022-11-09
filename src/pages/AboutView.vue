68ddb21b81dbf90fdde2edd1f1c565e1433d90e7
Feng-Fred <fred.shanghai.feng+2022@gmail.com>
2022-11-09 22:10:13 +0800

    验证组件在vue3下的使用

---
+++ src/pages/AboutView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about about2">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<style>
+@import url("./style.scss");
+
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
